687b9257c0bab52c4f263c5758e2a31478bba782
lcaldeira <caldeira.live@gmail.com>
2024-04-19 14:50:52 -0300

    toy dataset generation + minor changes in llna scripts

--- tests/llna_with_torch.py
+++ tests/llna_with_torch.py
@@ -15,21 +15,21 @@ from src.na.llna import LLNA
 # %% make instance of Life-Like NA
 
 automaton = LLNA(resolution=5, x=[1,2,3], y=[0,4])
-automaton.rule
+print('Rule:', automaton.rule)
 
 # %% Make instance of E-R network
 
-L = 3 # what is the meaning of L?
+initial_configs = 3
 
 G = ig.Graph.Erdos_Renyi(n=100, p=0.2)
 N = len(G.vs) # vertices
 E = tc.tensor([ e.tuple for e in G.es ]).T # edges
-h0 = tc.randint(0, 2, (L, N)) # initial configuration
+h0 = tc.randint(0, 2, (initial_configs, N)) # initial configuration
 
 # %% add network to LLNA
 
 H = automaton(E, h0, 50) # evolve over 50 timesteps
-H.shape
+print('Shape of TEP matrix:', H.shape)
 
 # %% plot TEP
 # Q: how are these ordered?
