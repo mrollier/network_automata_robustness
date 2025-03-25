1d8d0365295c977d8f6c21a432f40dabfe3896b8
Michiel Rollier <64779859+mrollier@users.noreply.github.com>
2025-03-25 03:27:27 +0100

    Nice exploration of different LLCAs on three different topologies

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+def get_ugent_colors_dict():
+    ugent_colors = {
+    'UGent Blue': '#1E64C8',
+    'UGent Yellow': '#FFD200',
+    'White': '#FFFFFF',
+    'Black': '#000000',
+    'LW': '#F1A42B',  # Faculty of Arts and Philosophy
+    'RE': '#DC4E28',  # Faculty of Law and Criminology
+    'WE': '#2D8CA8',  # Faculty of Sciences
+    'GE': '#E85E71',  # Faculty of Medicine and Health Sciences
+    'EA': '#8BBEE8',  # Faculty of Engineering and Architecture
+    'EB': '#AEB050',  # Faculty of Economics and Business Administration
+    'DI': '#825491',  # Faculty of Veterinary Medicine
+    'PP': '#FB7E3A',  # Faculty of Psychology and Educational Sciences
+    'BW': '#27ABAD',  # Faculty of Bioscience Engineering
+    'FW': '#BE5190',  # Faculty of Pharmaceutical Sciences
+    'PS': '#71A860'   # Faculty of Political and Social Sciences
+    }
+    return ugent_colors
