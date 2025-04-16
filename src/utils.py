ccc85d83ae2fc3bfaaa5de8d45db8e3ec1c27dcf
Michiel Rollier <64779859+mrollier@users.noreply.github.com>
2025-04-16 03:33:10 +0200

    Make new Notebook for inspecting the effect of rule 488-464 on all networks in the database

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+from matplotlib.colors import ListedColormap
+
 def get_ugent_colors_dict():
     ugent_colors = {
     'UGent Blue': '#1E64C8',
@@ -17,3 +19,22 @@ def get_ugent_colors_dict():
     'PS': '#71A860'   # Faculty of Political and Social Sciences
     }
     return ugent_colors
+
+def get_ugent_cmap():
+    ugent_colors_dict = get_ugent_colors_dict()
+    ugent_white = ugent_colors_dict['White']
+    ugent_blue = ugent_colors_dict['UGent Blue']
+    cmap = ListedColormap([ugent_white, ugent_blue])  # 0: white, 1: blue
+    return cmap
+
+def get_heart_shaped_grid():
+    """
+    Generates a heart-shaped grid of points.
+    Returns a list of tuples representing the coordinates of the points in the grid.
+    """
+    heart_shape = []
+    for x in range(-10, 11):
+        for y in range(-10, 11):
+            if (x**2 + (y - (x**2)**0.5)**2 <= 1):
+                heart_shape.append((x, y))
+    return heart_shape
